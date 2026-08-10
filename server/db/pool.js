f84f16495df5a338fd571d95fad89f5a5c79698e
Pubg121205 <86605975+Pubg121205@users.noreply.github.com>
2026-08-11 02:17:22 +0700

    Update pool.js

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,16 +2,15 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "160.191.243.50",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "tgtusnakesite_graduation_user",
-  password: process.env.DB_PASSWORD || "@14513563",
-  database: process.env.DB_NAME || "tgtusnakesite_graduation",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  charset: "utf8mb4"
+    waitForConnections: true,
+    connectionLimit: 10,
+    charset: "utf8mb4"
 });
 
 module.exports = pool;
