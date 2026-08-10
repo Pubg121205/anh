8d23b4b4fb09f9572918a7e0f766c1f29f54bd63
Pubg121205 <86605975+Pubg121205@users.noreply.github.com>
2026-08-11 01:52:02 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,17 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "160.191.243.50",
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER || "tgtusnakesite_graduation_user",
+  password: process.env.DB_PASSWORD || "@14513563",
+  database: process.env.DB_NAME || "tgtusnakesite_graduation",
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  charset: "utf8mb4"
+});
+
+module.exports = pool;
